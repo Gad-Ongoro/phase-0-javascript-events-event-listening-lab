4a50fa3a3b74b2e78207d9373d6befb76e312684
Gad-Ongoro <gadongoro1@gmail.com>
2023-09-28 19:06:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function alerter() {
+        alert("I was clicked");
+    }
+
+    input.addEventListener('click', alerter);
 }
